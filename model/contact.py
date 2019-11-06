d65a44cb02ae00abf7ffee9150e1de6f32b62a54
nastya <ostnas95@gmail.com>
2019-11-06 22:55:41 +0700

    Параметризованы тесты для создания групп и контактов

--- model/contact.py
+++ model/contact.py
@@ -32,7 +32,7 @@ class Contact:
         self.all_phones_from_home_page=all_phones_from_home_page
 
     def __repr__(self):
-        return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
+        return "%s:%s:%s:%s:%s" % (self.id, self.firstname, self.lastname, self.home, self.phone2)
 
     def __eq__(self, other):
         return (self.id is None or other.id is None or self.id == other.id) and self.firstname == other.firstname and self.lastname == other.lastname
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [ Contact(firstname="", lastname="", home="", phone2="")] + [
+    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 15),
+        home=random_string("home", 10), phone2=random_string("phone2", 12))
+    for i in range(5)
+]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="John", middlename="Smith", lastname="Black", nickname="black_johny",
-                               title="Title", company="The Greatest Company", address="Somewhere, 123/2, 8214499",
-                               home="22341", mobile="17263234242323432434234", work="82133728342", email="23@mail.com",
-                               email2="hello", homepage="www.google.com", bday="4", bmonth="September", byear="1990",
-                               address2="Secondary address", phone2="2", notes="No notes")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
