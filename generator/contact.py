ab157cde647b1ef4d889c25041eeb2909981e431
nastya <ostnas95@gmail.com>
2019-11-16 19:16:09 +0700

    Реализованы генераторы тестовых данных и загрузка тестовых данных из файлов

---
+++ generator/contact.py
@@ -0,0 +1,39 @@
+import random
+import string
+from model.contact import Contact
+import os.path
+import jsonpickle
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [ Contact(firstname="", lastname="", home="", phone2="")] + [
+    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 15),
+        home=random_string("home", 10), phone2=random_string("phone2", 12))
+    for i in range(5)
+]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(testdata))
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,21 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-import random
-import string
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-testdata = [ Contact(firstname="", lastname="", home="", phone2="")] + [
-    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 15),
-        home=random_string("home", 10), phone2=random_string("phone2", 12))
-    for i in range(5)
-]
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
