e269d80c18cb01a08ef471eb41c6f1dd6148ed8b
nastya <ostnas95@gmail.com>
2019-11-20 21:27:21 +0700

    Оптимизация для предусловий в удалении контакта из группы

--- test/test_delete_contact_from_group.py
+++ test/test_delete_contact_from_group.py
@@ -7,12 +7,20 @@ def test_delete_contact_from_group(app, orm):
         app.contact.create(Contact(firstname = "NameName"))
     if len(orm.get_group_list()) == 0:
         app.group.create(Group(name = "groupname"))
-    group = random.choice(orm.get_group_list())
-    contacts_in_group = orm.get_contacts_in_group(group)
-    if len(contacts_in_group) == 0:
-        contact = random.choice(orm.get_contact_list())
-        app.contact.add_contact_to_group(contact.id, group.id)
-        contacts_in_group.append(contact)
-    contact_to_delete_from_group = random.choice(contacts_in_group)
-    app.contact.delete_contact_from_group(contact_to_delete_from_group.id, group.id)
-    assert orm.contact_is_in_group(contact_to_delete_from_group, group) == False
+    all_groups = orm.get_group_list()
+    group_with_contact = None
+    contact_to_delete = None
+    for i, group in enumerate(all_groups):
+        contacts_in_group = orm.get_contacts_in_group(group)
+        if len(contacts_in_group) > 0:
+            contact_to_delete = random.choice(contacts_in_group)
+            group_with_contact = group
+            break
+        # all groups are empty, add contact to last group
+        elif i == len(all_groups) - 1:
+            contact = random.choice(orm.get_contact_list())
+            app.contact.add_contact_to_group(contact.id, group.id)
+            contact_to_delete = contact
+            group_with_contact = group
+    app.contact.delete_contact_from_group(contact_to_delete.id, group_with_contact.id)
+    assert orm.contact_is_in_group(contact_to_delete, group_with_contact) == False
